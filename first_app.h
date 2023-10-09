1525b7e039d769797f862c18c183cf180c585142
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-10-10 01:16:36 +0700

    update graphics pipeline

--- first_app.h
+++ first_app.h
@@ -2,6 +2,7 @@
 
 #include "window.h"
 #include "pipeline.h"
+#include "device.h"
 
 namespace rfe
 {
@@ -16,6 +17,12 @@ namespace rfe
 
 	private:
 		RfeWindow rfeWindow{WIDTH, HEIGHT, "Vulkan Window"};
-		RfePipeline rfePipeline{ "shader/simple_shader.vert.spv", "shader/simple_shader.frag.spv" };
+		RfeDevice rfeDevice{ rfeWindow };
+		RfePipeline rfePipeline{
+			rfeDevice, 
+			"shader/simple_shader.vert.spv", 
+			"shader/simple_shader.frag.spv", 
+			RfePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)
+		};
 	};
 }
--- pipeline.h
+++ pipeline.h
@@ -1,20 +1,56 @@
 #pragma once
 
+#include "device.h"
 #include <string>
 #include <vector>
 
 namespace rfe
 {
+	struct PipelineConfigInfo 
+	{
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
+
 	class RfePipeline
 	{
 	public:
-		RfePipeline(const std::string& vertFilePath, const std::string& fragFilePath);
+		RfePipeline(
+			RfeDevice& device,
+			const std::string& vertFilePath,
+			const std::string& fragFilePath,
+			const PipelineConfigInfo& configInfo
+		);
+		~RfePipeline();
+
+		RfePipeline(const RfePipeline&) = delete;
+		void operator=(const RfePipeline&) = delete;
+
+		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
 	private:
 		static std::vector<char> readFile(const std::string& filepath);
 		void createGraphicsPipeline(
 			const std::string& vertFilePath, 
-			const std::string& fragFilePath
+			const std::string& fragFilePath,
+			const PipelineConfigInfo& configInfo
 		);
+		void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+	private:
+		RfeDevice& rfeDevice;
+		VkPipeline graphicsPipeline;
+		VkShaderModule vertShaderModule;
+		VkShaderModule fragShaderModule;
 	};
 }
