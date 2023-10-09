65a01a8866301b129a20f096560949c227a8d846
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-10-09 20:41:18 +0700

    add vertex and fragment shader

---
+++ compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.261.1\Bin\glslc.exe shader\simple_shader.vert -o shader\simple_shader.vert.spv
+C:\VulkanSDK\1.3.261.1\Bin\glslc.exe shader\simple_shader.frag -o shader\simple_shader.frag.spv
+pause
